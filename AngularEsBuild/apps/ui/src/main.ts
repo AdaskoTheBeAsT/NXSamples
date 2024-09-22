a3787899129d9fdcf1e76080c2c0ec265188fbd0
Adam Pluciński <adaskothebeast@gmail.com>
2024-09-22 23:57:46 +0200

    upgrades

--- AngularEsBuild/apps/ui/src/main.ts
+++ AngularEsBuild/apps/ui/src/main.ts
@@ -1,14 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-// async function loadConfig() {
-//   const config = await import('./config') as any;
-//   // Use the config
-// }
 
-// loadConfig().then(() => {
 console.log(window._env_.clientId);
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+  console.error(err),
 );
-//});
