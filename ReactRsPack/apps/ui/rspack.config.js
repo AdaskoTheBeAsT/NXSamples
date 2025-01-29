a6eefc9882d55cd31a51276c42a221c9fb00e434
Adam Pluciński <adaskothebeast@gmail.com>
2025-01-29 04:31:32 +0100

    changes

--- ReactRsPack/apps/ui/rspack.config.js
+++ ReactRsPack/apps/ui/rspack.config.js
@@ -1,10 +1,25 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
+  config.plugins.push(
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      threshold: 1024,
+      compressionOptions: { level: 9 },
+    }),
+  );
+  config.plugins.push(
+    new CompressionPlugin({
+      algorithm: 'brotliCompress',
+      threshold: 1024,
+      compressionOptions: { level: 11 },
+    }),
+  );
   // Define multiple entry points
   config.entry = {
-    config: path.resolve(__dirname, 'src/config.ts'), // Separate entry for config
+    'env-config': path.resolve(__dirname, 'src/config.ts'), // Separate entry for config
     main: path.resolve(__dirname, 'src/main.tsx'), // Main application entry
   };
 
--- ReactVite/apps/ui/vite.config.ts
+++ ReactVite/apps/ui/vite.config.ts
@@ -4,10 +4,12 @@ import { ManualChunksOption, GetManualChunk } from 'rollup';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import viteCompression from 'vite-plugin-compression';
+import zlib from 'zlib';
 
 const manualChunks: GetManualChunk = (id, meta) => {
   if (id.includes('config.ts')) {
-    return 'config';
+    return 'env-config';
   }
 };
 
@@ -25,7 +27,28 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']), // Gzip
+    viteCompression({
+      algorithm: 'gzip',
+      threshold: 1025,
+      ext: '.gz',
+      compressionOptions: { level: 9 },
+    }),
+    // Brotli
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 1025,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+    }),
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {
